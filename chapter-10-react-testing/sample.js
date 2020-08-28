f2e4f48bc89b0f5389107c0ed3751154f7d49528
Billi Ramirez <ramirezbilly42@outlook.com>
2020-08-28 09:40:40 -0600

    adding some llinters and code formatters

---
+++ chapter-10-react-testing/sample.js
@@ -0,0 +1,25 @@
+const gnarName = "gnarly";
+
+const info = ({ file = __filename, dir = __dirname }) => (
+  <p>
+    {dir} : {file}
+  </p>
+);
+
+switch (gnar) {
+  default:
+    console.log("gnarly");
+    break;
+}
+
+function gnar() {
+  const [nickname, setNickname] = useState("dude");
+
+  return <h1>gnarly</h1>;
+}
+
+function Image() {
+  return <img src="/img.jpg" />;
+}
+
+console.log("Prettier Test");
